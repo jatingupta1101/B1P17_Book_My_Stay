3db1186b58c4eae5b3267862a3b62bb537fd76b6
jatingupta1101 <jatingupta1101@gmail.com>
2026-03-09 09:11:59 +0530

    Added main application entry class BookMyStayApp

---
+++ BookMyStayApp.java
@@ -0,0 +1,24 @@
+/**
+ * Book My Stay Application
+ *
+ * This class represents the main entry point of the Hotel Booking
+ * Management System. The application will evolve as new use cases
+ * are implemented in future commits.
+ *
+ * @author Jatin
+ * @version 1.0
+ */
+
+public class BookMyStayApp {
+
+    public static void main(String[] args) {
+
+        System.out.println("=================================");
+        System.out.println("        BOOK MY STAY APP         ");
+        System.out.println("      Hotel Booking System       ");
+        System.out.println("           Version 1.0           ");
+        System.out.println("=================================");
+
+        System.out.println("Welcome to the Hotel Booking Management System.");
+    }
+}
